492be7fbaeead50b53988f5c2dad3489df54cfdb
Felix Lam <flamht@gmail.com>
2023-08-04 13:36:18 +0800

    * hide sitemap for anti seo

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,17 +2,17 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    {
-      url: "https://fwdgo.vercel.app",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://fwdgo.vercel.app/about-us",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://fwdgo.vercel.app/join-us",
-      lastModified: new Date(),
-    },
+    // {
+    //   url: "https://fwdgo.vercel.app",
+    //   lastModified: new Date(),
+    // },
+    // {
+    //   url: "https://fwdgo.vercel.app/about-us",
+    //   lastModified: new Date(),
+    // },
+    // {
+    //   url: "https://fwdgo.vercel.app/join-us",
+    //   lastModified: new Date(),
+    // },
   ];
 }
